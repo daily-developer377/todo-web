962248e3fc2f99a14583b5a00db519bafef843eb
daily-developer377 <nandhusunil377@gmail.com>
2022-10-03 15:38:12 +0530

    todoweb

---
+++ main.js
@@ -0,0 +1,26 @@
+// html elements
+
+let kitchenInput = document.getElementById("kitchen-input");
+let addBtn = document.getElementById("add-btn");
+let kitchenItemsList = document.getElementById("kitchen-items-list");
+
+// step 2
+// add kitchenitems
+
+function addKitchenItems() {
+    let kitchenInputData = kitchenInput.value;
+
+    //create dom elements now
+    let li = document.createElement("li");                   //creating the li
+    li.innerText = kitchenInputData;                        //giving values to the li
+    li.style.cssText = 'animation-name: slideIn';           // we can create the css styles using js
+    kitchenItemsList.appendChild(li);
+    kitchenInput.value = '';                                //to reset the value in enter kitchen items list textbox
+    kitchenInput.focus();
+    console.log(li);
+}
+
+// step 1
+// add an  event listener to the button
+
+addBtn.addEventListener('click', addKitchenItems);
